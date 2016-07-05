bd8671d2ad0d83d752494c4e57dcf58724a4abe2
Toni Lähdekorpi <toni@lygon.net>
2016-07-05 13:30:19 +0300

    Better locale support for setup wizard
    Added UTF-8 to locales and remove any existing ones if set to ENV.

--- vendor/ValoaApplication/Controllers/Setup/SetupController.php
+++ vendor/ValoaApplication/Controllers/Setup/SetupController.php
@@ -271,6 +271,9 @@ class SetupController extends \Webvaloa\Application
         }
 
         $locale = getenv('LANG');
+        // Remove encoding from locale string
+        preg_match("/([^\.]+)[^\.]/",$locale,$locale);
+        $locale = $locale[0];
 
         $config = "<?php\n";
         $config .= "namespace Webvaloa;\n\n";
@@ -289,8 +292,9 @@ class SetupController extends \Webvaloa\Application
         $config .= "\n";
         $config .= '}';
         $config .= "\n\n";
-        $config .= "putenv('LANG={$locale}');\n";
-        $config .= "setlocale(LC_MESSAGES, '{$locale}');\n";
+        $config .= "putenv('LANG={$locale}.UTF-8');\n";
+        $config .= "setlocale(LC_ALL, '{$locale}.UTF-8');\n";
+        $config .= "setlocale(LC_MESSAGES, '{$locale}.UTF-8');\n";
 
         file_put_contents($configFile, $config);
 
